7bf46a5b1d9b611637d969d044946d25dae1c19b
AlexeyNikitin01 <lyosha-2001@mail.ru>
2025-10-30 12:44:13 +0300

    edu: перенес коннекты в main

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	initRedis "bot/internal/adapters/cache/redis"
+	"bot/internal/adapters/db/postres"
 	"bot/internal/domain/distpatcher"
 	"bot/internal/domain/user"
 	"bot/internal/domain/userQuestion"
 	"context"
+	"github.com/aarondl/sqlboiler/v4/boil"
+	"github.com/golang-migrate/migrate/v4"
+	postgresMigrate "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/pkg/errors"
+	"github.com/redis/go-redis/v9"
 	"gopkg.in/telebot.v3"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 
@@ -56,4 +63,58 @@ func waitForShutdown(bot *telebot.Bot, d domain.UseCases) {
 
 	log.Println("Приложение завершено")
 	os.Exit(0)
+}
+
+func ConnectDB(cfg *cfg.PG) {
+	db, err := postres.OpenConnectPostgres(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	boil.SetDB(db)
+	boil.DebugMode = cfg.DebugPG
+
+	// авто-миграции
+	driver, err := postgresMigrate.WithInstance(db.DB, &postgresMigrate.Config{
+		MigrationsTable: "schema_migrations",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations/postgres",
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal(err)
+	}
+
+	log.Println("Migrations applied successfully")
+}
+
+func ConnectBot(token string) *telebot.Bot {
+	pref := telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := telebot.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return b
+}
+
+func ConnectRedis(ctx context.Context, cfg *cfg.Redis) *redis.Client {
+	r, err := initRedis.NewClientRedis(ctx, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r
 }
